9d39960ca2a7363506af5a4e97eade6f4c19e494
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2025-12-06 20:17:29 +0800

    feat: added card content

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/ui/button'
+import Card from './components/ui/card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <Button />
+        <Card/> 
       </div>
     </>
   )
